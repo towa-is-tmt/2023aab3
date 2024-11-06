b5a941d370b620c798ca0cae8cda8ae308d1634b
towa-is-tmt <jefferytcc13@gmail.com>
2024-11-06 12:02:28 +0800

    week09

---
+++ week09/week09-3..cpp
@@ -0,0 +1,13 @@
+//1491. Average Salary Excluding the Minimum and Maximum Salary
+
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        sort(salary.begin(), salary.end());
+        double total =0;
+        for(int i=1; i<salary.size()-1; i++){
+            total += salary[i];
+        }
+        return total / (salary.size()-2);
+    }
+};
